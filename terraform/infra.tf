929ecb6cd151281c4aa62dd65b33ab1fa1216540
ramsyed123 <rameez786cool@gmail.com>
2024-12-23 07:20:20 +0000

    deploy4

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "countinstance" {
   ami           = "ami-0e2c8caa4b6378d8c"  
   instance_type = "t2.micro"
   tags = {
-    Name = countinstance
+   Name = "countinstance-${count.index + 1}"
   }
 }
 
